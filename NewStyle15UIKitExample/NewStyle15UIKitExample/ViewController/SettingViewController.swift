8fdb4f41e9da8bb4329c88f5797630ca3f4a01f1
Fumiya Sakai <just1factory@gmail.com>
2021-06-26 17:15:53 +0900

    Project内部の調整

---
+++ NewStyle15UIKitExample/NewStyle15UIKitExample/ViewController/SettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.swift
+//  NewStyle15UIKitExample
+//
+//  Created by 酒井文也 on 2021/06/26.
+//
+
+import UIKit
+
+final class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }    
+}
--- NewStyle15UIKitExample/NewStyle15UIKitExampleTests/NewStyle15UIKitExampleTests.swift
+++ NewStyle15UIKitExample/NewStyle15UIKitExampleTests/NewStyle15UIKitExampleTests.swift
@@ -29,5 +29,4 @@ class NewStyle15UIKitExampleTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
