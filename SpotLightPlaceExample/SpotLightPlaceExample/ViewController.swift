f25aa600f9a732f46311aee5683ea29c72e821ef
fumiyasac <just1factory@gmail.com>
2023-07-02 13:12:44 +0900

    iOS15以降の機能で実現できることを利用した事例

---
+++ SpotLightPlaceExample/SpotLightPlaceExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotLightPlaceExample
+//
+//  Created by 酒井文也 on 2023/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
